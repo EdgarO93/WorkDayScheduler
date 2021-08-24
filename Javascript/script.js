dac0176021ffa2e05bf05eb283a450a39738fdb0
Edgar Ortega <edort93@gmail.com>
2021-08-23 22:49:09 -0500

    added current date to top of page and pseudo code

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,20 @@
 //define variables
+var today= moment().format("MMMM D, YYYY");
+// probrably add variables to each hour to maybe make each a form?
 
+// Date Set (month number, year), Could maybe add time
+$("#currentDay").text(" Today is  " + today);
+
+
+//set up the time blocks (likely using bootstrap in html)
+
+
+//function to save each input and add to local storage
+
+
+
+
+//function to change the color based on time, maybe some kind of conditional statment
+// to check time (if current time >block turn gray, else if time= red, else green)
+
+//click event for save button

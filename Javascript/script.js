1127b07c1b239ffb1cb8c71156457f8608c018fc
Edgar Ortega <edort93@gmail.com>
2021-08-24 23:11:12 -0500

    added more pseudocode and started a click event

--- Javascript/script.js
+++ Javascript/script.js
@@ -6,15 +6,19 @@ var today= moment().format("MMMM D, YYYY");
 $("#currentDay").text(" Today is  " + today);
 
 
-//set up the time blocks (likely using bootstrap in html)
+
+//need to figure out how to compare string value to time
+//function to change the color based on time, maybe some kind of conditional statment
+// to check time (if current time < time make red, else if turn current time >time green else nothing)
+//color could be done by assigned an id or class and then using css to change color 
+
+//function changeColor(){ }
 
 
 //function to save each input and add to local storage
 
-
-
-
-//function to change the color based on time, maybe some kind of conditional statment
-// to check time (if current time >block turn gray, else if time= red, else green)
-
 //click event for save button
+$(safeBtn).on("click", function () {
+  //function for saving list
+    
+});

4f185bede11c7173ee36b527917587ef54cbee69
Edgar Ortega <edort93@gmail.com>
2021-08-25 23:21:52 -0500

    converted id for each hour into time and added before after to compare moments

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,47 +1,42 @@
 //define variables
 var today = moment().format("MMMM D, YYYY");
-// probrably add variables to each hour to maybe make each a form?
+
 //sets time  
-var time = moment().format("hh:mm:ss");
+var time = moment().format("HH:mm");
 
 // Date Set (month number, year),
 $("#currentDay").text(" Today is  " + today);
 
-//timer
-// Sets timer
+
+// Sets timer for clock
 timer = setInterval(function () {
-    var time = moment().format("hh:mm:ss");
+    var time = moment().format("HH:mm");
     $('#clock').text(time);
     $('textarea').each(function () {
-        
-        var blockTime= $(this).attr("id");
-        console.log(blockTime)
-        if (blockTime < time) {
-            $("blockT").addClass("past")
+
+        var blockTime = $(this).attr("id");
+        var blockHour = moment(blockTime, "HH:mm")
+        // console.log(blockHour)
+
+        if (moment(time, "HH:mm").isAfter(blockHour, "HH:mm")) {
+
+            $(".blockT").addClass("past").removeClass("blockT");
+
         }
-        else if (blockTime > time) {
-            $("blockT").addClass("future")
+        else if (moment(time, "HH:mm").isBefore(blockHour, "HH:mm")) {
+            $(".blockT").addClass("future").removeClass("blockT");
+
         }
+        else {
+            $(".blockT").addClass("present").removeClass("blockT");
+        }
+
     })
-   
+
 
 }, 1000);
 
 
-//need to figure out how to compare string value to time
-//function to change the color based on time, maybe some kind of conditional statment
-// to check time (if current time < time make red, else if turn current time >time green else nothing)
-//color could be done by assigned an id or class and then using css to change color 
-
-// function changeColor(){ 
-// if (currenttime<time){
-//    $("currenttime")addClass("past")
-
-// } else if (currenttime>time){
-//     $("currenttime")addClass("future")
-// }
-
-// };
 
 //function to save each input and add to local storage
 
